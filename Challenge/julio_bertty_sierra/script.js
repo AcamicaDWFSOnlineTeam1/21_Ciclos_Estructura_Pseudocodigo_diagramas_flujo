274e35c2afdd6dda927375276b74ff4d22bedddc
jbcoolness <julio.bertty@gmail.com>
2021-03-03 15:33:08 -0500

    Solucion Challenge

---
+++ Challenge/julio_bertty_sierra/script.js
@@ -0,0 +1,12 @@
+// Ciclo for
+console.log('**Ciclo con FOR**')
+for (let index = 1; index < 11; index++) {
+    console.log("Vuelta N°: "+ index);
+}
+
+console.log('**Ciclo con While**')
+let i = 1
+while (i < 11) {
+    console.log("Vuelta N°: "+ i);
+    i++;
+}
